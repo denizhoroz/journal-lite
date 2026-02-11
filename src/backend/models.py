d7d1fd2279bdb33e2050d9aeb4d7be291d6015b6
Deniz Eren Horoz <denizhoroz.ofcl@gmail.com>
2026-02-12 01:26:19 +0300

    created database structure

---
+++ src/backend/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, Date, Text, String
+from .database import Base
+
+class JournalEntry(Base):
+    __tablename__ = "journal_entries"
+
+    id = Column(Integer, primary_key=True)
+    date = Column(Date, nullable=False)
+    tab_name = Column(String(100), nullable=False)
+    entry = Column(Text, nullable=False)
+
+class JournalEvent(Base):
+    __tablename__ = "journal_events"
+
+    id = Column(Integer, primary_key=True)
+    date = Column(Date, nullable=False)
+    events = Column(Text, nullable=False)
---
+++ src/backend/repository.py
@@ -0,0 +1,56 @@
+from datetime import date
+from .database import SessionLocal
+from .models import JournalEntry, JournalEvent
+
+class Repository:
+
+    # JournalEntry
+    def add_entry(self, entry_date: date, tab_name: str, content: str):
+        with SessionLocal() as session:
+            entry = JournalEntry(
+                date=entry_date,
+                tab_name=tab_name,
+                entry=content
+            )
+            session.add(entry)
+            session.commit()
+
+    def get_entry(self, entry_date: date, tab_name: str):
+        with SessionLocal() as session:
+            return (
+                session.query(JournalEntry)
+                .filter((JournalEntry.date == entry_date) & 
+                        (JournalEntry.tab_name == tab_name))
+                .all()
+            )
+
+    def delete_entry(self, entry_id: int):
+        with SessionLocal() as session:
+            entry = session.get(JournalEntry, entry_id)
+            if entry:
+                session.delete(entry)
+                session.commit()
+
+    def delete_tab():
+        pass
+
+    def delete_date():
+        pass
+
+    # JournalEvent
+    def add_event(self, event_date: date, content: str):
+        with SessionLocal() as session:
+            event = JournalEvent(
+                date=event_date,
+                events=content
+            )
+            session.add(event)
+            session.commit()
+
+    def get_events_by_date(self, event_date: date):
+        with SessionLocal() as session:
+            return (
+                session.query(JournalEvent)
+                .filter(JournalEvent.date == event_date)
+                .all()
+            )
